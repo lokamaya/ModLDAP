da07abdff79526bb714049b7e2a91982f05d9109
Zaenal <public@lokamaya.com>
2021-06-20 22:18:08 +0700

    Update modldapuser.class.php
    Updated LDAP group bindings. This part came form Dmitry Shram. Thx u.

--- core/components/modldap/model/modldapuser/modldapuser.class.php
+++ core/components/modldap/model/modldapuser/modldapuser.class.php
@@ -155,8 +155,7 @@ class modLDAPUser extends modUser {
             $this->syncProfileFromLDAP();
             
             //Insert to Groups
-            $this->autoAddGroupToUserLDAP();
-            $this->autoSyncGroupFromUserLDAP();
+            $this->autoUpdateUserGroupLDAP();
             
             return true;
         }
@@ -169,11 +168,12 @@ class modLDAPUser extends modUser {
     * @param array $entries
     * @return void
     **/
-    public function updateUserFromLDAP($entries) {        
+    public function updateUserFromLDAP($entries) {
         $this->set_ldap_entries($entries);
         unset($entries);
         
-        $old_remote_data = unserialize($this->get('remote_data'));
+//        $old_remote_data = unserialize($this->get('remote_data'));
+        $old_remote_data = $this->get('remote_data');
         $new_remote_data = $this->ldap_entries;
 
         if ($old_remote_data != $new_remote_data) {
@@ -187,8 +187,7 @@ class modLDAPUser extends modUser {
                 $this->save();
                 
                 //Update user group
-                $this->autoAddGroupToUserLDAP();
-                $this->autoSyncGroupFromUserLDAP();
+                $this->autoUpdateUserGroupLDAP();
             }
         }
     }
@@ -252,61 +251,83 @@ class modLDAPUser extends modUser {
     }
     
     /**
-    * Auto-Add MODX User's Group
+    * Auto-Add MODX User's Group and auto sync LDAP User's Group
     * 
     * @return void
     **/
-    public function autoAddGroupToUserLDAP() {
+    public function autoUpdateUserGroupLDAP() {
         $modx_group_add  = $this->getOption(modLDAP::AUTOADD_USERGROUPS, false, false);
-        $modx_group_name = $this->getOption(modLDAP::AUTOADD_USERGROUPS_NAME, null, '');
-        $modx_group_role = $this->getOption(modLDAP::AUTOADD_USERGROUPS_ROLE, null, '');
+        $ldap_group_add  = $this->getOption(modLDAP::LDAP_GROUP_ADD, false, false);
         
-        if (!$modx_group_add) return;
+        $modx_groups_name = $this->getOption(modLDAP::AUTOADD_USERGROUPS_NAME, null, '');
+        $modx_groups = array_map("trim", explode(',', $modx_groups_name));
+
+        $modx_group_roles = $this->getOption(modLDAP::AUTOADD_USERGROUPS_ROLE, null, '');
+        $modx_roles = array_map("trim", explode(',', $modx_group_roles));
+
+        $ldap_role  = trim($this->getOption(modLDAP::LDAP_GROUP_ROLE, null, 'Publisher'));
         
-        $error = false;
-        if (empty($modx_group_name)) {
-            $error = true;
-            $this->xpdo->log(xPDO::LOG_LEVEL_INFO, "[modLDAPUser] " . modLDAP::AUTOADD_USERGROUPS_NAME . " is empty!");
-        }
-        if (empty($modx_group_role)) {
-            $error = true;
-            $this->xpdo->log(xPDO::LOG_LEVEL_INFO, "[modLDAPUser] " . modLDAP::AUTOADD_USERGROUPS_ROLE . " is empty!");
-        }
+        if ($modx_group_add || $ldap_group_add)
+            $user_groups = @$this->getUserGroupNames();
+
+
+        $updated = false;
         
-        if ($error) return false;
         
-        $modx_group   = explode(',', $modx_group_name);
-        $modx_roles   = explode(',', $modx_group_role);
-        $_default_role = (count($modx_roles) < count($modx_group));
-        
-        foreach ($modx_group as $i => $grp) {
-            if ($_default_role) {
-                @$this->joinGroup(trim($grp),trim($modx_roles[0]), $i+1);
-            } else {
-                @$this->joinGroup(trim($grp),trim($modx_roles[$i]), $i+1);
+        if ($modx_group_add) {
+            if (empty($modx_group_name))
+                $this->xpdo->log(xPDO::LOG_LEVEL_INFO, "[modLDAPUser] " . modLDAP::AUTOADD_USERGROUPS_NAME . " is empty!");
+            elseif (empty($modx_group_role))
+                $this->xpdo->log(xPDO::LOG_LEVEL_INFO, "[modLDAPUser] " . modLDAP::AUTOADD_USERGROUPS_ROLE . " is empty!");
+            else {
+                $_default_role = count($modx_roles) < count($modx_groups);
+                $modx_groups_index = array_flip($modx_groups);
+                
+                $user_groups_remove = array_diff($user_groups, $modx_groups);
+                $user_groups_add = array_diff($modx_groups, $user_groups);
+
+                foreach ($user_groups_add as $i => $grp) {
+                    $role = $modx_roles[$_default_role ? 0 : $modx_groups_index[$grp]];
+                    @$this->joinGroup($grp, $role, $i+1);
+                    $updated = true;
+                }
+                foreach ($user_groups_remove as $grp) {
+                    @$this->leaveGroup(trim($grp));
+                    $updated = true;
+                }
+            }
+        } else
+            $modx_groups = array();
+
+
+        if ($ldap_group_add) {
+            $ldap_group = $this->getManyLdapEntryGroup("memberof", true);
+            
+            $q = $this->xpdo->newQuery('modUserGroup'); 
+            $q->select(['name']);
+            $q->prepare();
+            $q->stmt->execute();
+            $all_user_groups = $q->stmt->fetchAll(PDO::FETCH_COLUMN);
+    
+            $valid_groups = array_merge(array_intersect ($ldap_group, $all_user_groups), $modx_groups);
+            
+            $user_groups_remove = array_diff($user_groups, $valid_groups);
+            $user_groups_add = array_diff($valid_groups, $user_groups);
+            
+            foreach ($user_groups_add as $i => $grp) {
+                @$this->joinGroup(trim($grp), $ldap_role, $i+1);
+                $updated = true;
+            }
+                
+            foreach ($user_groups_remove as $grp) {
+                @$this->leaveGroup(trim($grp));
+                $updated = true;
             }
         }
         
-        return;
-    }
-    
-    /**
-    * Sync LDAP User's Group (not implemented)
-    * 
-    * @return void
-    **/
-    public function autoSyncGroupFromUserLDAP() {
-        return true; //not implemented
-        $ldap_group_add  = $this->getOption(modLDAP::LDAP_GROUP_ADD, false, false);
-        $ldap_group  = array();
-        $ldap_roles  = trim($this->getOption(modLDAP::LDAP_GROUP_ROLE, null, 'Member'));
-        
-        if (!$ldap_group_add) return true;
-        
-        $ldap_group = $this->getManyLdapEntryGroup($entries);
-        foreach ($modx_group as $i => $grp) {
-            @$this->joinGroup(trim($grp), $modx_roles, $i+10);
-        }
+        //Clear the cache
+        if ($updated)
+            $this->xpdo->cacheManager->refresh(array('system_settings' => array()));
     }
     
     /**
@@ -400,17 +421,10 @@ class modLDAPUser extends modUser {
         
         // Get entries ...
         $entries = $this->ldap_entries;
-        if (!is_array($entries)) return array();
-        if (empty($entries)) return array();
-        
         // Get mapping field
-        $cn = $field;
-        $maps = $this->fields_mapping;
-        if (array_key_exists($field,$maps)) {
-            $cn = $this->fields_mapping[$field];
-        }
-        
-        if (empty($cn)) return array();
+        $cn = $this->fields_mapping[$field] ?: $field;
+
+        if (!is_array($entries) || empty($entries) || empty($cn)) return array();
         
         // Get the results
         $_result = array();
@@ -428,7 +442,7 @@ class modLDAPUser extends modUser {
                     $_result[] = $row;
                 }
             } else {
-                $this->xpdo->log(xPDO::LOG_LEVEL_INFO, " - getManyLdapEntry($cn) unknwon field!" );
+                $this->xpdo->log(xPDO::LOG_LEVEL_INFO, " - getManyLdapEntry($cn) unknown field!" );
             }
         }
         
@@ -453,7 +467,6 @@ class modLDAPUser extends modUser {
                 }
             }
         }
-        
         return $_result;
     }
     
